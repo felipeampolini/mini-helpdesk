4b8a787fea90a65af1ba4efb8e611b32e3c6a3f6
Felipe <felipeampolini@hotmail.com>
2026-01-04 15:23:26 -0300

    chore: addiciona porta vite para desenvolvimento

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // permite acesso externo (de fora do container)
+        port: 5173,       // porta do Vite
+        strictPort: true,
+        cors: {
+            origin: 'http://localhost:8000',
+            credentials: true,
+        },
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
