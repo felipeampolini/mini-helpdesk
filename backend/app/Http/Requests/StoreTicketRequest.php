44ef1c956f577b0ff211c08285a797e9b3424233
Felipe <felipeampolini@hotmail.com>
2026-01-05 20:13:28 -0300

    chore: remove codigo nao usado

--- backend/app/Http/Requests/StoreTicketRequest.php
+++ backend/app/Http/Requests/StoreTicketRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreTicketRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return auth()->user()->can('create', \App\Models\Ticket::class);
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
